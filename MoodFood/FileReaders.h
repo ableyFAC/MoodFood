6a00c2bfa4c13dadeee1786b4bf9cab3b583ff43
Ethan Vu <pumpkin673@gmail.com>
2025-09-18 20:33:57 -0700

    Remade the FileReaders.h functions, renamed restaurants_database.txt to differentiate it from the other menu_txt files, removed menu_database.txt file for efficiency

--- MoodFood/FileReaders.h
+++ MoodFood/FileReaders.h
@@ -10,34 +10,25 @@
 
 using namespace std;
 
-const string RESTAURANTS_FILE = "restaurants_database.txt";
-const string MENU_DATABASE = "menu_database.txt";
+const string RESTAURANTS_FILE = "--restaurants_database.txt";
 
-void createRestaurantAndMenuList(ifstream& restaurantFile, ifstream& menuDatabase, RestaurantsList& aRestaurantList)
+void createRestaurantAndMenuList(ifstream& restaurantFile, RestaurantsList& aRestaurantList)
 {
+    string menuFile;
     int mood = 0;
     string restaurant;
     double rating = 0.0;
-    MenuList tempForNow;
 
-    while (restaurantFile >> mood >> restaurant >> rating)
+    while (restaurantFile >> menuFile >> mood >> restaurant >> rating)
     {
-        aRestaurantList.addRestaurant(mood, restaurant, rating, tempForNow);
-    }
+        ifstream theMenuFile;
+        theMenuFile.open(menuFile);
 
-    vector<string> menuDatabaseVector;
-    string line;
-
-    while (getline(menuDatabase, line))
-    {
-        menuDatabaseVector.push_back(line);
-    }
-
-    for (int i = 0; i < menuDatabaseVector.size(); ++i)
-    {
-        ifstream infile;
-        string temp = menuDatabaseVector.at(i);
-        infile.open(temp);
+        if (!theMenuFile)
+        {
+            cerr << menuFile << " does NOT exist" << endl;
+            exit(1);
+        }
 
         string category;
         string item;
@@ -45,17 +36,14 @@ void createRestaurantAndMenuList(ifstream& restaurantFile, ifstream& menuDatabas
 
         MenuList aMenu;
 
-        while (infile >> category >> item >> price)
+        while (theMenuFile >> category >> item >> price)
         {
             Category newCat = aMenu.stringToEnum(category);
             aMenu.addMenuItem(newCat, Menu(newCat, item, price));
         }
 
-        string restaurantName = temp.substr(0, temp.find("_"));
+        aRestaurantList.addRestaurant(mood, restaurant, rating, aMenu);
 
-        auto it = aRestaurantList.findRestaurant(restaurantName);
-
-        it->second.setMenuList(aMenu);
     }
 }
 
@@ -71,21 +59,9 @@ void getFileData(RestaurantsList& aRestaurantList)
         exit(1);
     }
 
-    ifstream menuDatabase;
-
-    menuDatabase.open(MENU_DATABASE);
-
-    if (!menuDatabase)
-    {
-        cerr << MENU_DATABASE << " does NOT exist" << endl;
-        exit(1);
-    }
-
-
-    createRestaurantAndMenuList(restaurantFile, menuDatabase, aRestaurantList);
+    createRestaurantAndMenuList(restaurantFile, aRestaurantList);
 
     restaurantFile.close();
-    menuDatabase.close();
 
 }
 
